8e044d2e422f70f830b49b96c843f0b61bd1dcb9
UndergroundSea <eliel-ing@hotmail.fi>
2021-11-10 16:13:17 +0200

    Ensimmäinen tehtävä

--- Osa1/Osa1.a/src/App.js
+++ Osa1/Osa1.a/src/App.js
@@ -1,15 +1,55 @@
 import React from 'react'
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  
+
+const Header = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
       <p>
-        {a} plus {b} is {a + b}
+        {props.part1} {props.exercises1}
       </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
