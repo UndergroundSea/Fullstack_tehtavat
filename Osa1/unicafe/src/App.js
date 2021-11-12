cdd40aa1c84518c59c950eeadf8ebe3169d03025
UndergroundSea <eliel-ing@hotmail.fi>
2021-11-12 12:55:18 +0200

    Tehtävä 1.7

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -13,15 +13,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(((good+1) - bad) / (all+1))
+    setPositive((good+1)/(all+1)*100)
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good - bad) / (all+1))
+    setPositive((good)/(all+1)*100)
   }
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage((good - (bad+1)) / (all+1))
+    setPositive((good)/(all+1)*100)
   }
 
   return (
@@ -30,10 +42,14 @@ const App = () => {
       <Button handleClick={handleGood} text="good" />
       <Button handleClick={handleNeutral} text="neutral" />
       <Button handleClick={handleBad} text="bad" />
+
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
