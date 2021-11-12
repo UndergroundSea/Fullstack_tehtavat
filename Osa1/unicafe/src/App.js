4a06d79e8ce690b51097179ecc8383fdb49f0930
UndergroundSea <eliel-ing@hotmail.fi>
2021-11-12 13:21:46 +0200

    Tehtävä 1.9

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -7,7 +7,14 @@ const Button = ({ handleClick, text }) => (
 )
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
