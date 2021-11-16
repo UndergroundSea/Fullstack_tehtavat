32edb1e6f778045cfd0d8a44a2686d81faed8997
UndergroundSea <eliel-ing@hotmail.fi>
2021-11-16 20:46:40 +0200

    Tehtävä 2.4

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -13,9 +13,9 @@ const Course = ({course}) => {
 const Header = (props) => {
   return (
     <div>
-      <h1>
+      <h2>
         {props.course}
-      </h1>
+      </h2>
     </div>
   )
 }
@@ -48,44 +48,69 @@ const Total = ({parts}) => {
   
   return (    
     <div>      
-      <p>     
+      <h4>     
         Number of exercises {total}
-      </p>
+      </h4>
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Exam',
-        exercises: 5,
-        id: 4
-      }
-    ]
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  const final = []
+
+  for (const course of courses) {
+    final.push(<Course key={course.id} course={course} />)
   }
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {final}
     </div>
   )
 }
