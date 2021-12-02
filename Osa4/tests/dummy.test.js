d745da2ad33cba0281c0fb260c259719bc870660
UndergroundSea <eliel-ing@hotmail.fi>
2021-12-02 13:56:51 +0200

    Tehtävä 4.4

--- Osa4/tests/dummy.test.js
+++ Osa4/tests/dummy.test.js
@@ -1,8 +1,25 @@
 const listHelper = require('../utils/list_helper')
+const { listWithOneBlog, blogs } = require('./test_lists')
+
+const oneBlog = listWithOneBlog
+const manyBlogs = blogs
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(oneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has many blogs equal the likes of these', () => {
+        const result = listHelper.totalLikes(manyBlogs)
+        expect(result).toBe(36)
+    })
 })
--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    let sum = 0
+    for (let i = 0; i < blogs.length; i++) {
+        sum += blogs[i].likes
+    }
+    return sum
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
