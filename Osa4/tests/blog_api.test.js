074907b3f8e06771e134f8601c8564e2f00120db
UndergroundSea <eliel-ing@hotmail.fi>
2021-12-03 14:50:08 +0200

    Tehtävä 4.12

--- Osa4/tests/blog_api.test.js
+++ Osa4/tests/blog_api.test.js
@@ -49,17 +49,90 @@ test('there are ten blogs', async () => {
 
 test('the first blog is named React patterns', async () => {
     const response = await api.get('/api/blogs')
-    console.log('HEI')
     expect(response.body[0].title).toBe('React patterns')
 })
 
-test('blogs are defined by id', async () => {
-    console.log('TRYING')
+test('blogs can be added with post', async () => {
+    const newBlog = {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
     const response = await api.get('/api/blogs')
-    console.log('RESPONSE:', response)
-    console.log('RESPONSE BODY:', response.body[0])
-    console.log('RESPONSE ID:', response.body[0].id)
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toContain(
+        'Canonical string reduction'
+    )
+})
+
+test('blogs are defined by id', async () => {
+    const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
+})
+
+test('likes become 0 if not specified', async () => {
+    const newBlog = {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const likes = response.body.map(r => r.likes)
+
+    expect(likes[likes.length - 1]).toBe(0)
+})
+
+describe('title or url missing', () => {
+    test('returns 400 if title is missing', async () => {
+        const newBlog = {
+            _id: "5a422bc61b54a676234d17fc",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 2,
+            __v: 0
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
+
+    test('returns 400 if url is missing', async () => {
+        const newBlog = {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            likes: 2,
+            __v: 0
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 })
 
 afterAll(() => {
